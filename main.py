75b4cb55518231576b286808987189e3312f10fb
DonatasNoreika <donoras@gmail.com>
2023-12-06 08:52:28 +0200

    +Neleistų spėti skaičiaus už ribų

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ print("Pasirinkite spėjamo skaičiaus ribas")
 nuo = int(input("Nuo: "))
 iki = int(input("Iki: "))
 
-sugeneruotas = random.randint(nuo, iki)
+sugeneruotas = random.randint(nuo + 1, iki - 1)
 print(sugeneruotas)
 counter = 0
 
 while True:
     print(f"Skaičiaus ribos: nuo {nuo} iki {iki}")
     spejamas = int(input("Spėjamas skaičius: "))
+    if spejamas <= nuo or spejamas >= iki:
+        print("Spėjimas iš ribų")
+        continue
     counter += 1
     if spejamas > sugeneruotas:
         print("Mažiau")
