2b2b803171e86a393f1306bf01fa9bf887991aee
DonatasNoreika <donoras@gmail.com>
2023-12-06 08:25:56 +0200

    +Įdėti skaitiklį, kuris fiksuotų spėjimų skaičių ir praneštų jį žaidimo pabaigoje

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import random
 
 sugeneruotas = random.randint(1, 100)
-print(sugeneruotas)
+# print(sugeneruotas)
+counter = 0
 
 while True:
     spejamas = int(input("Spėjamas skaičius: "))
+    counter += 1
     if spejamas > sugeneruotas:
         print("Mažiau")
     if spejamas < sugeneruotas:
         print("Daugiau")
     if spejamas == sugeneruotas:
-        print("Atspėjote!")
+        print(f"Atspėjote! Spėjimų skaičius: {counter}")
         break
