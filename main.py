0ebbe75e59b88042583ec64eeec48d15421a0837
DonatasNoreika <donoras@gmail.com>
2023-12-06 08:36:53 +0200

    +Rodyti likusių spėjimų ribas

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ print(sugeneruotas)
 counter = 0
 
 while True:
+    print(f"Skaičiaus ribos: nuo {nuo} iki {iki}")
     spejamas = int(input("Spėjamas skaičius: "))
     counter += 1
     if spejamas > sugeneruotas:
         print("Mažiau")
+        iki = spejamas
     if spejamas < sugeneruotas:
         print("Daugiau")
+        nuo = spejamas
     if spejamas == sugeneruotas:
         print(f"Atspėjote! Spėjimų skaičius: {counter}")
         break
